59563f3e811709b0aab32782f0d43b4e6f56024a
Epharra Mendoza <epharramendoza@epharras-mbp.attlocal.net>
2020-10-13 20:55:09 -0500

    Logic to fetch data from v1/summaryactivity

--- React/app/src/App.js
+++ React/app/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home';
+import Summary from './components/Summary';
+import Navigation from './components/Navigation';
+
+class App extends Component{
+    render(){
+        return (
+            <BrowserRouter>
+            <div>
+            <Navigation />
+                <Switch>
+                <Route path="/" component={Home} exact/>
+                <Route path="/summary" component={Summary}/>
+                </Switch>
+            </div>
+            </BrowserRouter>
+        );
+    }
 }
-
 export default App;
