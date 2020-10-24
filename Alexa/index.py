587cf30c56b03e0510cae661ea527495c455c7e3
James Smith <jamessmith@Jamess-MacBook-Pro.local>
2020-10-23 20:37:44 -0500

    switched to post json

--- Alexa/index.py
+++ Alexa/index.py
@@ -9,11 +9,11 @@ import datetime
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '?????????????????'
-app.config['MYSQL_HOST'] = '?'
-app.config['MYSQL_USER'] = '?'
-app.config['MYSQL_PASSWORD'] = '?'
-app.config['MYSQL_DB'] = '?'
+app.config['SECRET_KEY'] = '03948nn3498cnb34095c734n597'
+app.config['MYSQL_HOST'] = 'derekjsmith.mysql.pythonanywhere-services.com'
+app.config['MYSQL_USER'] = 'derekjsmith'
+app.config['MYSQL_PASSWORD'] = 'abcde12345'
+app.config['MYSQL_DB'] = 'derekjsmith$default'
 
 @app.route('/', methods=['GET','POST'])
 @app.route('/index', methods=['GET','POST'])
@@ -25,7 +25,7 @@ def index():
             sleep = request.args['sleep']
             symptoms = request.args['symptoms']
             time = request.args['time']
-            cnx = mysql.connector.connect(user='?',database='?',host='?',password='?')
+            cnx = mysql.connector.connect(user='derekjsmith',database='derekjsmith$default',host='derekjsmith.mysql.pythonanywhere-services.com',password='abcde12345')
             cursor = cnx.cursor()
             query = "INSERT INTO CS5500 (id,feelings,sleep,symptoms,time) VALUES ('%d','%s','%s','%s','%s')" % (identifier,feelings,sleep,symptoms,time)
             cursor.execute(query)
@@ -34,5 +34,16 @@ def index():
         except Exception as e:
             return '''<html><head></head><body>Error.</body></html>'''
         return '''<html><head></head><body>Database updated.</body></html>'''
+    elif request.method=='POST':
+        try:
+            json = request.get_json(force=True)
+            cnx = mysql.connector.connect(user='derekjsmith',database='derekjsmith$default',host='derekjsmith.mysql.pythonanywhere-services.com',password='abcde12345')
+            cursor = cnx.cursor()
+            query = "INSERT INTO json (data) VALUES ('%s')" % (json)
+            cursor.execute(query)
+            cnx.commit()
+            cursor.close()
+        except Exception as e:
+            return '''<html><head></head><body>Error.</body></html>'''
     return '''<html><head></head><body>No get request.</body></html>'''
 
