d344c8eaa58460b96c51c738420759ffc9f559b8
Sam Trenter <sntrenter@gmail.com>
2020-11-10 16:07:50 -0600

    Updates

---
+++ precache-manifest.678c8da12cb6d954dfe6b1854447a865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f364535891ac2107681c5efaa99769",
+    "url": "/FA2020GroupProject/index.html"
+  },
+  {
+    "revision": "9c019ec0b9e9ea803cd9",
+    "url": "/FA2020GroupProject/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "9c019ec0b9e9ea803cd9",
+    "url": "/FA2020GroupProject/static/js/2.954f5754.chunk.js"
+  },
+  {
+    "revision": "5a3e1ef6b0b0d9e746135ec264ab328a",
+    "url": "/FA2020GroupProject/static/js/2.954f5754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d515c50d2f8672ed33",
+    "url": "/FA2020GroupProject/static/js/main.89cfda0d.chunk.js"
+  },
+  {
+    "revision": "d76b895ac4da9cce7fbb",
+    "url": "/FA2020GroupProject/static/js/runtime-main.3bf027e6.js"
+  }
+]);
