e3f7d888e0c53f346ea75b75f91f2e62558a64e0
Sam Trenter <sntrenter@gmail.com>
2020-11-18 20:00:55 -0600

    changed top level component to Dashboard layout

--- React/app/src/App.js
+++ React/app/src/App.js
@@ -6,13 +6,17 @@ import GlobalStyles from './components/GlobalStyles';
 // import 'src/mixins/chartjs';
 import theme from './theme/theme';
 import routes from '../src/routes';
+import DashboardLayout from './DashboardLayout/DashboardLayout'
+
+//<GlobalStyles />
 
 const App = () => {
   const routing = useRoutes(routes);
 
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
+      
+      <DashboardLayout />
       {routing}
     </ThemeProvider>
       
