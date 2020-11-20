166877bc5854fd1dbaea0261cc7c73b6c2dc7d7f
happy <17792773+jml-happy@users.noreply.github.com>
2020-11-19 19:58:54 -0600

    remove 'O' and '0' (zero) from patient id

--- Backend/random_patient_code.py
+++ Backend/random_patient_code.py
@@ -1,8 +1,9 @@
 import random
 import string
 def get_random_patient_code():
-  prefix = random.sample(string.ascii_uppercase, 3)
-  suffix = random.sample(range(10), 3)
+  _letters = string.ascii_uppercase.replace("O", '')
+  prefix = random.sample(_letters, 3)
+  suffix = random.sample(range(1, 10), 3)
   code_list = [str(x) for x in prefix + suffix]
   code = ''.join(code_list)
   return code
